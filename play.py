76e3a79cc8e3228fad85934260743f31eb9ef289
Andrew Booker <improvizone@googlemail.com>
2019-08-30 00:26:35 +0100

    moving creation of ramping stereo image into a class

--- play.py
+++ play.py
@@ -5,26 +5,39 @@ import soundfile as sf
 import sys
 import math
 
-
-filename = sys.argv[1]
-(data, fs) = sf.read(filename, dtype="float32")
-
-sampleCount = len(data)
-ramp = math.floor(sampleCount / 5.0)
+import random
 
 
-stereo = []
-for i in range(sampleCount):
-	sample = data[i]
-	if (i < ramp):
-		sample *= (1.0 * i / ramp)
+class RawSample():
+	def __init__(self, file):
+		(data, sampleRate) = sf.read(file, dtype="float32")
+		self.data = data
 
-	if (i > (sampleCount - ramp)):
-		sample *= (1.0 * (sampleCount - i) / ramp)
 
-	left = sample * 0.9
-	right = sample * 0.1
-	stereo.append([left, right])
+class Sample():
+	def __init__(self, data):
+		self.pan = random.random()
+		sampleCount = len(data)
+		ramp = math.floor(sampleCount / (6.0 * random.random()))
+		
+		self.stereo = []
+		for i in range(sampleCount):
+			sample = data[i]
+			if (i < ramp):
+				sample *= (1.0 * i / ramp)
 
-sd.play(stereo, fs)
+			if (i > (sampleCount - ramp)):
+				sample *= (1.0 * (sampleCount - i) / ramp)
+
+			left = sample * self.pan
+			right = sample * (1.0 - self.pan)
+			self.stereo.append([left, right])
+	
+file = RawSample(sys.argv[1])
+sample1 = Sample(file.data)
+sd.play(sample1.stereo, 44100)
 sd.wait()
+print("done")
+	
+
+
