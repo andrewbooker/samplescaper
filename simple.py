779340e8207a019d5bbd40446e8a075a133765e7
Andrew Booker <improvizone@googlemail.com>
2019-09-05 21:17:27 +0100

    playing all the files in a given directory

--- simple.py
+++ simple.py
@@ -4,39 +4,38 @@ import pygame as pg
 import time
 import random
 import math
+import sys
 
 pg.mixer.init()
 pg.init()
 
-a1Note = pg.mixer.Sound("d:\\Samples\\Voice\\Long\\48\\shortSample003.wav")
-a2Note = pg.mixer.Sound("d:\\Samples\\Voice\\Long\\48\\shortSample007.wav")
+from os import listdir
+from os.path import isfile, join
 
+path = sys.argv[1]
+files = [join(path, f) for f in listdir(path) if isfile(join(path, f))]
+
+notes = [pg.mixer.Sound(f) for f in files]
 
 def frLen(sound, f):
-    return math.floor(a1Note.get_length() * 1000.0 * f)
-
+    return math.floor(sound.get_length() * 1000.0 * f)
 
 pg.mixer.set_num_channels(50)
 channels = []
 
 for i in range(10):
-    channel1 = pg.mixer.find_channel()
-    pan1 = random.random()
-    channel1.set_volume(1.0 - pan1, pan1)
-    channel1.play(a1Note, fade_ms=random.randint(frLen(a1Note, 0.1), frLen(a1Note, 0.3)))
-    channel1.fadeout(frLen(a1Note, 0.5))
-    channels.append(channel1)
+    n = i if i < len(notes) else i % len(notes)
+
+    channel = pg.mixer.find_channel()
+    pan = random.random()
+    note = notes[n]
+    channel.set_volume(1.0 - pan, pan)
+    channel.play(note, fade_ms=random.randint(frLen(note, 0.1), frLen(note, 0.3)))
+    channel.fadeout(frLen(note, 0.5))
+    channels.append(channel)
 
     time.sleep(0.3 + random.random())
-    
-    channel2 = pg.mixer.find_channel()
-    pan2 = random.random()
-    channel2.set_volume(1.0 - pan2, pan2)
-    channel2.play(a2Note, fade_ms=random.randint(frLen(a2Note, 0.1), frLen(a2Note, 0.3)))
-    channel2.fadeout(frLen(a1Note, 0.5))
-    channels.append(channel1)
- 
-    time.sleep(0.3 + random.random())
+
 print("created %d channels" % len(channels))
 pg.mixer.fadeout(5000)
 time.sleep(5)
