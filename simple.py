5a85f2bad688c48991c3fe5c578b2925ac74f863
Andrew Booker <improvizone@googlemail.com>
2019-09-05 23:16:17 +0100

    randomly playing every note in the scale

--- simple.py
+++ simple.py
@@ -12,31 +12,36 @@ pg.init()
 from os import listdir
 from os.path import isfile, join
 
-path = sys.argv[1]
-files = [join(path, f) for f in listdir(path) if isfile(join(path, f))]
+scale = [48, 50, 51, 53, 55, 56, 58]
 
+path = sys.argv[1]
+
+files = []
+for s in scale:
+    notePath = join(path, str(s))
+    files.extend([join(notePath, f) for f in listdir(notePath) if isfile(join(notePath, f))])
+
+print("found %d files" % len(files))
 notes = [pg.mixer.Sound(f) for f in files]
 
-def frLen(sound, f):
-    return math.floor(sound.get_length() * 1000.0 * f)
+def fractionLen(note, f):
+    return math.floor(note.get_length() * 1000.0 * f)
 
-pg.mixer.set_num_channels(50)
-channels = []
+pg.mixer.set_num_channels(30)
+end = time.time() + 60
 
-for i in range(10):
-    n = i if i < len(notes) else i % len(notes)
-
+while time.time() < end:
     channel = pg.mixer.find_channel()
-    pan = random.random()
-    note = notes[n]
-    channel.set_volume(1.0 - pan, pan)
-    channel.play(note, fade_ms=random.randint(frLen(note, 0.1), frLen(note, 0.3)))
-    channel.fadeout(frLen(note, 0.5))
-    channels.append(channel)
+    if channel is not None:
+        pan = random.random()
+        note = notes[random.randint(0, len(notes) - 1)]
+        channel.set_volume(1.0 - pan, pan)
+        channel.play(note, fade_ms = fractionLen(note, 0.4))
+        time.sleep(note.get_length() * 0.45)
+        channel.fadeout(fractionLen(note, 0.5))
+    else:
+        time.sleep(0.3)
 
-    time.sleep(0.3 + random.random())
-
-print("created %d channels" % len(channels))
 pg.mixer.fadeout(5000)
 time.sleep(5)
 print("done")
