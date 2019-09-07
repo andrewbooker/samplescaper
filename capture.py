ff9ed9d016813d28be898987eba9288d061f13d0
Andrew Booker <improvizone@googlemail.com>
2019-09-07 19:57:40 +0100

    alternative audio capture recorder that creates new file during sufficiently loud sections

--- capture.py
+++ capture.py
@@ -12,10 +12,58 @@ import os
 import keyboard
 import threading
 
+# utils
+
+import math
+
+
+def any(a, ommitting = []):
+    f = a[randint(0, len(a) - 1)]
+    if f in ommitting:
+        return any(a, ommitting)
+    return f
+
+
+class MovingAvg():
+    def __init__(self, size):
+        self.size = size
+        self.clear()
+
+    def clear(self):
+        self.values = []
+        self.avg = 0.0
+
+    def add(self, v):
+        self.avg += (v * 1.0 / self.size)
+        self.values.append(v)
+        if (len(self.values) > self.size):
+            p = self.values.pop(0)
+            self.avg -= (p * 1.0 / self.size)
+
+    def first(self):
+        return self.values[0]
+
+    def value(self):
+        return self.avg if (len(self.values) == self.size) else (self.avg * self.size / len(self.values))
+
+
+class AbsMovingAvg(MovingAvg):
+    def __init__(self, size):
+        MovingAvg.__init__(self, size)
+
+    def add(self, v):
+        self.avg += (abs(v) * 1.0 / self.size)
+        self.values.append(v)
+        if (len(self.values) > self.size):
+            p = self.values.pop(0)
+            self.avg -= (abs(p) * 1.0 / self.size)
+
+# audio capture
+
 class Buffer():
     def __init__(self):
         self.q = queue.Queue()
-    
+
     def make(self):
         def handle(indata, frames, time, status):
             self.q.put(indata.copy())
@@ -25,13 +73,13 @@ class CaptureAudio():
     def __init__(self, dev, cb):
         self.dev = dev
         self.cb = cb
-    
+
     def start(self, shouldStop):
         print("starting audio capture on %s" % self.dev)
         with sd.InputStream(samplerate=44100.0, device=self.dev, channels=1, callback=self.cb.make()):
             while not shouldStop.is_set():
                 time.sleep(1)
-                
+
             print("stopping audio capture on %s" % self.dev)
 
 class RecordAudio():
@@ -44,17 +92,61 @@ class RecordAudio():
         with sf.SoundFile("%s/%s.wav" % (self.loc, fn), mode="x", samplerate=44100, channels=1, subtype="PCM_16") as file:
             while not shouldStop.is_set():
                 file.write(self.buffer.q.get())
-         
+
+class RecordSamples():
+    def __init__(self, dirOut, buffer):
+        self.dirOut = "%s/%s" % (dirOut, datetime.datetime.fromtimestamp(now).strftime("%Y-%m-%d_%H%M%S"))
+        os.makedirs(self.dirOut)
+        self.buffer = buffer
+        self.out = None
+        self.state = 0
+        self.movingAvg5 = AbsMovingAvg(5)
+        self.movingAvg30 = AbsMovingAvg(30)
+        self.fn = 0
+        self.i = 0
+
+    def _readOneSample(self, v):
+        self.movingAvg5.add(v)
+        self.movingAvg30.add(v)
+
+        if (self.state == 2):
+            self.movingAvg5.clear()
+            self.movingAvg30.clear()
+            self.state = 0
+
+        if (self.state == 1 and self.movingAvg30.avg < 0.003):
+            print("ending sample at %d at %d" % (self.fn, self.i))
+            self.state = 2
+            self.out.close()
+            self.out = None
+            self.fn += 1
+
+        if (self.state == 0 and abs(v - self.movingAvg5.avg) > 0.25):
+            print("beginning sample %d at %d" % (self.fn, self.i))
+            self.out = sf.SoundFile("%s/sample%03d.wav" % (self.dirOut, self.fn), mode="x", samplerate=44100, channels=1, subtype="PCM_16")
+            self.state = 1
+
+        if (self.state == 1):
+            self.out.write(self.movingAvg5.first())
+
+    def start(self, shouldStop):
+        while not shouldStop.is_set():
+            data = self.buffer.q.get()
+            for d in data:
+                self._readOneSample(d)
+                self.i += 1
+
+
 buffer = Buffer()
 
-now = time.time()    
+now = time.time()
 outDir = sys.argv[1]
 if not os.path.exists(outDir):
     os.makedirs(outDir)
-    
+
 shouldStop = threading.Event()
 
-recording = RecordAudio(outDir, buffer)
+recording = RecordSamples(outDir, buffer)
 capture = CaptureAudio(1, buffer)
 
 captureThread = threading.Thread(target = capture.start, args = (shouldStop,), daemon = True)
