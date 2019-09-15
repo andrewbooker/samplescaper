643f73a43e48c386193b8316918e347211df32b8
Andrew Booker <improvizone@googlemail.com>
2019-09-15 23:10:58 +0100

    trying pitch bend for fun

--- midiPlayer.py
+++ midiPlayer.py
@@ -13,6 +13,10 @@ while (i != 127):
 	print("Playing %d" % i)
 	player.set_instrument(i) # a program change
 	player.note_on(64, 127, 0)
+
+	for p in range(-8192, 8192, 100):
+		player.pitch_bend(p, 0)
+		time.sleep(0.01)
 	time.sleep(2)
 	player.note_off(64, 127, 0)
 	i += 1
