6b45141774c3aee3908b09820cb3ce2b75f9d8ac
Andrew Booker <improvizone@gmail.com>
2020-04-10 17:58:47 +0100

    selecting first suitable audio device and prompting cmd line argument for sample directory if missing

--- simpleCapture.py
+++ simpleCapture.py
@@ -13,6 +13,17 @@ from multiprocessing import Value
 
 from utils.LoopableSample import LoopableSample
 
+def usableAudioDevices():
+    usable = {}
+
+    devs = sd.query_devices()
+    for d in range(len(devs)):
+        dev = devs[d]
+        if "USB" in dev["name"] and dev["default_samplerate"] == 44100:
+            usable[d] = dev["name"]
+
+    return usable
+
 # audio capture
 
 class Buffer():
@@ -62,15 +73,23 @@ class RecordSamples():
         if out is not None:
             stream.stop()
 
-buffer = Buffer()
+if len(sys.argv) < 2:
+    print("please supply output directory (will be created if necessary)")
+    exit()
+
 outDir = sys.argv[1]
+buffer = Buffer()
 if not os.path.exists(outDir):
     os.makedirs(outDir)
 sampleNumber = Value('i', int(sys.argv[2]) if len(sys.argv) > 2 else 48)
 shouldStop = threading.Event()
 shouldRecordClip = threading.Event()
 
-recording = RecordSamples(1, outDir, buffer) # "Microphone (Blue Snowball ), MME"
+devices = usableAudioDevices()
+audioDevice = [k for k in devices.keys()][0]
+print("using %s" % devices[audioDevice])
+
+recording = RecordSamples(audioDevice, outDir, buffer)
 recordThread = threading.Thread(target = recording.start, args = (sampleNumber, shouldStop, shouldRecordClip), daemon = True)
 
 print("ready to record to %s" % outDir)
