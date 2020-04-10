828a1166aea7363232b789256894c49884a63a33
Andrew Booker <improvizone@gmail.com>
2020-04-10 23:46:22 +0100

    hooking in sample recording to produce script to record samples from MIDI sound module

--- midiSampleGenerator.py
+++ midiSampleGenerator.py
@@ -2,6 +2,11 @@
 
 import pygame.midi as midi
 import time
+import sys
+import os
+
+from utils.sampleRecording import Controller
+from utils.sampleRecording import usableAudioDevices
 
 
 def getUsableOutputDevice():
@@ -12,6 +17,25 @@ def getUsableOutputDevice():
 		if isOutput and "USB" in str(info[1]):
 			return d
 	return None
+
+#### same as simpleCapture
+if len(sys.argv) < 2:
+    print("please supply output directory (will be created if necessary)")
+    exit()
+
+outDir = sys.argv[1]
+
+if not os.path.exists(outDir):
+    os.makedirs(outDir)
+
+
+devices = usableAudioDevices()
+audioDevice = [k for k in devices.keys()][0]
+print("using %s" % devices[audioDevice])
+startNumber = int(sys.argv[2]) if len(sys.argv) > 2 else 48
+
+controller = Controller(startNumber, audioDevice, outDir)
+####
 
 midi.init()
 device = getUsableOutputDevice()
@@ -24,13 +48,17 @@ if device is None:
 player = midi.Output(device, latency = 0)
 
 baseNote = 48
-for i in range(24):
+for i in range(12):
 	note = baseNote + i
+	controller.setNumber(note)
 	player.note_on(note, velocity=100, channel=0)
+	controller.toggleRecord(None)
 	time.sleep(2.0)
+	controller.toggleRecord(None)
 	player.note_off(note, velocity=0, channel=0)
 	time.sleep(1.0)
 
+controller.stopCapture(None)
 player.close()
 
 del player
