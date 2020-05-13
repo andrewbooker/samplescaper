e92e1448d7a04d3365d37fbcb327c80cf7dcf38c
Andrew Booker <improvizone@gmail.com>
2020-05-13 23:42:11 +0100

    using mediautils to provide midi device instead of using own lookup

--- midiSampleGenerator.py
+++ midiSampleGenerator.py
@@ -1,6 +1,5 @@
 #!/usr/bin/env python
 
-import pygame.midi as midi
 import time
 import os
 import sys
@@ -14,18 +13,10 @@ def checkImport(lib):
 
 checkImport("mediautils")
 from mediautils.audiodevices import UsbAudioDevices
+from mediautils.mididevices import UsbMidiDevices, MidiOut
 
 from utils.sampleRecording import Controller
 
-
-def getUsableOutputDevice():
-	c = midi.get_count()
-	for d in range(c):
-		info = midi.get_device_info(d)
-		isOutput = (info[3] == 1)
-		if isOutput and "USB" in str(info[1]):
-			return d
-	return None
 
 #### same as simpleCapture
 if len(sys.argv) < 2:
@@ -48,29 +39,21 @@ cycles = int(sys.argv[3]) if len(sys.argv) > 3 else 1
 controller = Controller(baseNote, audioDevice, outDir)
 ####
 
-midi.init()
-device = getUsableOutputDevice()
-
-if device is None:
-	print("No USB MIDI devices found")
-	midi.quit()
-	exit()
-	
-player = midi.Output(device, latency = 0)
+midiDevices = UsbMidiDevices()
+midiOut = MidiOut(midiDevices)
 
 for c in range(cycles):
 	for i in range(18):
 		note = baseNote + i
 		controller.setNumber(note)
-		player.note_on(note, velocity=100, channel=0)
+		midiOut.io.note_on(note, velocity=100, channel=0)
 		controller.toggleRecord(None)
 		time.sleep(4.0)
 		controller.toggleRecord(None)
-		player.note_off(note, velocity=0, channel=0)
+		midiOut.io.note_off(note, velocity=0, channel=0)
 		time.sleep(4.0)
 
 controller.stopCapture(None)
-player.close()
 
-del player
-midi.quit()
+del midiOut
+del midiDevices
