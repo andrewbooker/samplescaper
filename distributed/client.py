73263bc5abba3201a042c305a3923c0e4a06b3bf
Andrew Booker <improvizone@gmail.com>
2020-07-05 23:54:29 +0100

    simple python client playing mono sample

--- distributed/client.py
+++ distributed/client.py
@@ -2,37 +2,26 @@
 
 import pygame as pg
 import requests
-import random
 import time
 
-
-
-
-import shutil
-import requests
-
-audioFile = "./thing.wav"
+pg.mixer.init(frequency=44100, size=-16, channels=1, buffer=1024)
+pg.init()
 
 url = "http://localhost:3064"
 response = requests.get(url, stream=True)
-with open(audioFile, "wb") as dump:
-    shutil.copyfileobj(response.raw, dump)
+note = pg.mixer.Sound(response.raw.read())
 del response
-
 
 fadeInSecs = 1
 fadeOutSecs = 10
 
-
-pg.mixer.init(frequency=44100, size=-16, channels=2, buffer=1024)
-pg.init()
-
-note = pg.mixer.Sound(audioFile)
 channel = pg.mixer.find_channel()
-pan = random.random()
-channel.set_volume(1.0 - pan, pan)
+channel.set_volume(1.0)
 channel.play(note, loops = -1, fade_ms = fadeInSecs * 1000)
 time.sleep(fadeInSecs)
 channel.fadeout(fadeOutSecs * 1000)
 time.sleep(fadeOutSecs)
+
+pg.mixer.quit()
+pg.quit()
 
