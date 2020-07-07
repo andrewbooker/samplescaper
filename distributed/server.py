abf459ff88ca0274fb5ad1945dd536af85f506dc
Andrew Booker <improvizone@gmail.com>
2020-07-07 19:08:42 +0100

    serving randomly selected file from given directory

--- distributed/server.py
+++ distributed/server.py
@@ -3,18 +3,24 @@
 from http.server import HTTPServer, BaseHTTPRequestHandler
 import threading
 import sys
-import datetime
 import time
 import urllib
 import os
+import random
 
 
-audioFile = sys.argv[1]
+def nextAudioFile():
+    audioFileDir = sys.argv[1]
+    notes = [50, 52, 53, 55, 57, 58, 60, 62, 64, 65]
+    note = notes[random.randint(0, len(notes) - 1)]
+    noteDir = os.path.join(audioFileDir, str(note))
+    files = os.listdir(noteDir)
+    return os.path.join(os.path.join(noteDir, files[random.randint(0, len(files) - 1)]))
 
 
 class AudioFileServer(BaseHTTPRequestHandler):
-    
     def do_GET(self):
+        audioFile = nextAudioFile()
         size = os.stat(audioFile).st_size
         with open(audioFile, "rb") as loopFile:
             self.send_response(200)
@@ -36,12 +42,11 @@ class StoppableServer:
         self.httpd.shutdown()
         self.httpd.server_close()
         print("stopped")
-        
-        
+
+random.seed()
 port = 3064
 server = StoppableServer(port)
 thread = threading.Thread(target=server.start, args=(), daemon=True)
-
 
 thread.start()
 done = False
