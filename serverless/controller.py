fe806286e90ac3bcb723537789807e2ec855f3ef
Andrew Booker <improvizone@gmail.com>
2020-09-21 21:32:53 +0100

    sending current system volume after get request

--- serverless/controller.py
+++ serverless/controller.py
@@ -3,20 +3,28 @@
 from http.server import HTTPServer, BaseHTTPRequestHandler
 import os
 import json
+import re
 
 class SystemVolumeControl(BaseHTTPRequestHandler):
+    def _sendVol(self, vol):
+        self.send_response(200)
+        self.send_header("Content-Type", "application/json")
+        self.end_headers()
+
+        d = {"volume": vol}
+        self.wfile.write(json.dumps(d).encode("utf-8"))
+
+    def do_GET(self):
+        sv = os.popen("amixer sget 'Digital'").read().split("\n")[-2]
+        self._sendVol(int(re.search("\[(\d+)%\]", sv).group(1)))
+
     def do_PUT(self):
         l = int(self.headers.get("content-length", 0))
         data = self.rfile.read(l)
         obj = json.loads(data.decode())
         vol = int(obj["volume"])
         os.system("amixer sset 'Digital' %d%%" % vol)
-        self.send_response(200)
-        self.send_header("Content-Type", "application/json")   
-        self.end_headers()
-        
-        d = {"volume": vol}
-        self.wfile.write(json.dumps(d).encode("utf-8"))
+        self._sendVol(vol)
 
     def do_POST(self):
         os.system("shutdown now")
