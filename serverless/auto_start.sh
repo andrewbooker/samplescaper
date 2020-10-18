6ac548576b47fbabeb131e550566ee9251de420c
Andrew Booker <improvizone@gmail.com>
2020-10-18 21:02:51 +0100

    switching back to playing prepared loops so these can be stashed for assembling into a recording

--- serverless/auto_start.sh
+++ serverless/auto_start.sh
@@ -10,7 +10,10 @@ fi
 rm -rf ~/Music/pool
 mkdir -p ~/Music/pool/factory
 mkdir -p ~/Music/pool/raw
+mkdir -p ~/Music/pool/looped
+mkdir -p ~/Music/pool/played
 
 python ~/Documents/samplescaper/synth/synth.py ~/Music/pool &
+python ~/Documents/samplescaper/synth/loop.py ~/Music/pool &
 python ~/Documents/samplescaper/serverless/controller.py &
-python ~/Documents/samplescaper/serverless/playRaw.py ~/Music/pool &
+python ~/Documents/samplescaper/serverless/playPrepped.py ~/Music/pool &
