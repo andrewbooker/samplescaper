b8529f60dc4496181dc789ba667f2f125a9b7b7d
Andrew Booker <improvizone@gmail.com>
2020-10-31 17:15:31 +0000

    adding pitch shifting pieces to auto_start

--- serverless/auto_start.sh
+++ serverless/auto_start.sh
@@ -2,11 +2,14 @@
 
 rm -rf ~/Music/pool
 mkdir -p ~/Music/archives
+mkdir -p ~/Music/samples
 mkdir -p ~/Music/pool/factory
 mkdir -p ~/Music/pool/raw
 mkdir -p ~/Music/pool/looped
 
 python ~/Documents/samplescaper/synth/synth.py ~/Music/pool &
 python ~/Documents/samplescaper/synth/loop.py ~/Music/pool &
+python ~/Documents/samplescaper/synth/pitch.py ~/Music/samples ~/Music/pool/raw &
+python ~/Documents/samplescaper/synth/recycle.py ~/Music/pool/raw ~/Music/samples &
 python ~/Documents/samplescaper/serverless/controller.py ~/Music/pool &
 ~/Documents/samplescaper/recordAmbient.sh &
