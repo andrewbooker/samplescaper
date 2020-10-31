d54018d03fa525e09307f30a01a10cbe10a5835f
Andrew Booker <improvizone@gmail.com>
2020-10-31 12:52:33 +0000

    trying out a sinusoidal pitch shift an capping output length to 10s

--- synth/pitch.py
+++ synth/pitch.py
@@ -20,12 +20,27 @@ def stretch():
     return s
     
 class Linear():
-    def __init__(self, dy, dx, c):
-        self.gradient = dy / dx
-        self.intercept = c
-    
+    def __init__(self, dataLength):
+        self.intercept = stretch()
+        self.gradient = (stretch() - self.intercept) / dataLength
+
     def at(self, i):
         return self.intercept + (i * self.gradient)
+
+    def describe(self):
+        return "linear_%3f" % (self.intercept + self.gradient)
+
+class Sine():
+    def __init__(self):
+        self.f = random.random() / 22050
+        self.offset = 2 * random.random() * math.pi
+        self.amplitude = 0.5 + (0.5 * random.random())
+
+    def at(self, i):
+        return 1.2 + (self.amplitude * math.sin(self.offset + (i * self.f)))
+
+    def describe(self):
+        return "sine_%3f" % self.f
 
 inDir = sys.argv[1]
 outDir = sys.argv[2]
@@ -33,28 +48,28 @@ outDir = sys.argv[2]
 while True:
     f = nextAudioFileFrom(inDir)
     print("using", os.path.basename(f))
-    data, samplerate = sf.read(f)
-    
-    sd = len(data)
+    data, sampleRate = sf.read(f)
+
     out = []
-    s1 = stretch()
-    s2 = stretch()
+    dataLength = len(data)
     
-    print(s1, "to", s2)
-    sa = 0.5 * (s1 + s2)
-    rng = int(sd * sa)
-    fi = Linear(s2 - s1, rng, s1)
-    for i in range(rng):
+    fi = Linear(dataLength) if random.random() < 0.2 else Sine()
+    print(fi.describe())
+    done = False
+    i = 0
+    while not done:
         p = i / fi.at(i)
         p0 = math.floor(p)
-        if p0 >= sd:
-            break
-        p1 = math.ceil(p)
-        if p1 >= sd:
-            p1 = p0
-        e = p - p0
-        out.append(((1.0 - e) * data[p0]) + (e * data[p1]))
+        if p0 >= dataLength or i > (10 * sampleRate):
+            done = True
+        else:
+            p1 = math.ceil(p)
+            if p1 >= dataLength:
+                p1 = p0
+            e = p - p0
+            out.append(((1.0 - e) * data[p0]) + (e * data[p1]))
+        i += 1
 
-    sf.write(os.path.join(outDir, "%s_%.3f.wav" % (os.path.basename(f), sa)), out, samplerate)
+    sf.write(os.path.join(outDir, "%s_%s.wav" % (os.path.basename(f), fi.describe())), out, sampleRate)
     time.sleep(2)
 
