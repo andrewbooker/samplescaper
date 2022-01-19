8ee8318594db7872d1012649e8cf6357422b8db4
Andrew Booker <improvizone@gmail.com>
2022-01-19 18:57:43 +0000

    introducing second recycler process for looped items as well as less frequent raw items

--- serverless/start.sh
+++ serverless/start.sh
@@ -1,17 +1,20 @@
 #!/bin/bash
 
 rm -rf ~/Music/pool
-rm -rf ~/Music/samples
+rm -rf ~/Music/samples*
 mkdir -p ~/Music/archives
-mkdir -p ~/Music/samples
+mkdir -p ~/Music/samples1
+mkdir -p ~/Music/samples2
 mkdir -p ~/Music/pool/factory
 mkdir -p ~/Music/pool/raw
 mkdir -p ~/Music/pool/looped
 
-tonic=56
+tonic=66
 
-python ~/Documents/samplescaper/synth/modify.py ~/Music/samples ~/Music/pool/raw &
-python ~/Documents/samplescaper/synth/recycle.py ~/Music/pool/raw ~/Music/samples &
+python ~/Documents/samplescaper/synth/modify.py ~/Music/samples1 ~/Music/pool/looped &
+python ~/Documents/samplescaper/synth/modify.py ~/Music/samples2 ~/Music/pool/raw &
+python ~/Documents/samplescaper/synth/recycle.py ~/Music/pool/looped ~/Music/samples1 10 &
+python ~/Documents/samplescaper/synth/recycle.py ~/Music/pool/raw ~/Music/samples2 33 &
 python ~/Documents/samplescaper/synth/synth.py ~/Music/pool $tonic &
 python ~/Documents/samplescaper/synth/loop.py ~/Music/pool &
 python ~/Documents/samplescaper/serverless/controller.py ~/Music/pool ~/Documents/samplescaper/serverless/config.json &
--- synth/recycle.py
+++ synth/recycle.py
@@ -7,17 +7,18 @@ import time
 
 
 def nextAudioFileFrom(poolDir):
-    files = [f for f in filter(lambda f: len(f.split(".")) == 2 and "wav" in f, os.listdir(poolDir))]
+    files = [f for f in filter(lambda f: len(f.split(".")) == 2 and "wav" in f and "__" not in f, os.listdir(poolDir))]
     if len(files) == 0:
         return None
     return os.path.join(poolDir, files[random.randint(0, len(files) - 1)])
 
 inDir = sys.argv[1]
 outDir = sys.argv[2]
+interval = int(sys.argv[3])
     
 while True:
     f = nextAudioFileFrom(inDir)
     if f is not None:
         print("copying", f, "to", outDir)
         os.system("cp %s %s" % (f, outDir))
-        time.sleep(20)
+        time.sleep(interval)
