ffe0f65ec61769f4321f3ed8b24c83ba64e8faf5
Andrew Booker <improvizone@gmail.com>
2022-04-12 21:18:43 +0100

    preventing file overrun and randomising a couple of constants

--- synth/arpeggiate.py
+++ synth/arpeggiate.py
@@ -13,12 +13,13 @@ rawDir = os.path.join(inDir, "raw")
 factoryDir = os.path.join(inDir, "factory")
 outDir = os.path.join(inDir, "looped")
 allFiles = os.listdir(rawDir)
+fIdx = len(os.listdir(outDir))
 random.shuffle(allFiles)
 
-toUse = allFiles[:6]
+toUse = allFiles[:random.randint(3, 11)]
 
 inf = [sf.read(os.path.join(rawDir, f))[0] for f in toUse]
-inFiles = [(len(f), f) for f in inf]
+inFiles = [[len(f), f, 0] for f in inf]
 
 sampleRate = 44100
 
@@ -27,15 +28,17 @@ unitSampleLength = int(sampleRate * (0.05 + (0.1 * random.random())))
 print("unitSampleLength", unitSampleLength)
 pulses = 20
 
-f = 4
-fqfn = os.path.join(factoryDir, "arpeggiated_%d.wav" % f)
+
+fqfn = os.path.join(factoryDir, "arpeggiated_%d.wav" % fIdx)
 outfile = sf.SoundFile(fqfn, "w", samplerate=sampleRate, channels=1)
 
 print("writing", fqfn)
 for p in range(pulses):
-    startIdx = p * unitSampleLength
     for fd in inFiles:
-        outfile.write([fd[1][s] for s in range(startIdx, startIdx + unitSampleLength)])
+        outfile.write([fd[1][s] for s in range(fd[2], fd[2] + unitSampleLength)])
+        fd[2] += unitSampleLength
+        if (fd[2] + unitSampleLength) > fd[0]:
+            fd[2] = 0
 
 outfile.close()
 print("moving to live pool after %.2fs" % (time.monotonic() - start))
