48b85de4f727d7f98c88eb7d2d99272d2840727c
Andrew Booker <improvizone@gmail.com>
2022-04-15 15:27:32 +0100

    swapping in arpeggiator

--- serverless/start.sh
+++ serverless/start.sh
@@ -3,17 +3,18 @@
 rm -rf ~/Music/pool
 rm -rf ~/Music/samples*
 mkdir -p ~/Music/archives
-mkdir -p ~/Music/samples1
-mkdir -p ~/Music/samples2
+#mkdir -p ~/Music/samples1
+#mkdir -p ~/Music/samples2
 mkdir -p ~/Music/pool/factory
 mkdir -p ~/Music/pool/raw
 mkdir -p ~/Music/pool/looped
 
-python ~/Documents/samplescaper/synth/modify.py ~/Music/samples1 ~/Music/pool/looped ~/Documents/samplescaper/config.json &
-python ~/Documents/samplescaper/synth/modify.py ~/Music/samples2 ~/Music/pool/raw ~/Documents/samplescaper/config.json &
-python ~/Documents/samplescaper/synth/recycle.py ~/Music/pool/looped ~/Music/samples1 21.4 &
-python ~/Documents/samplescaper/synth/recycle.py ~/Music/pool/raw ~/Music/samples2 37.2 &
+#python ~/Documents/samplescaper/synth/modify.py ~/Music/samples1 ~/Music/pool/looped ~/Documents/samplescaper/config.json &
+#python ~/Documents/samplescaper/synth/modify.py ~/Music/samples2 ~/Music/pool/raw ~/Documents/samplescaper/config.json &
+#python ~/Documents/samplescaper/synth/recycle.py ~/Music/pool/looped ~/Music/samples1 21.4 &
+#python ~/Documents/samplescaper/synth/recycle.py ~/Music/pool/raw ~/Music/samples2 37.2 &
 python ~/Documents/samplescaper/synth/synth.py ~/Music/pool ~/Documents/samplescaper/config.json &
+python ~/Documents/samplescaper/synth/arpeggiate.py ~/Music/pool &
 python ~/Documents/samplescaper/synth/loop.py ~/Music/pool &
 python ~/Documents/samplescaper/serverless/controller.py ~/Music/pool ~/Documents/samplescaper/config.json &
 
