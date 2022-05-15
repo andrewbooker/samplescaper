a240daf64c5aa7522fa3f42822b17ac52fd648ba
Andrew Booker <improvizone@gmail.com>
2022-05-15 18:11:03 +0100

    beginnings of setup script for new device

---
+++ serverless/setup.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+
+sudo apt-get install fswebcam
+sudo apt-get install python-pygame
+sudo apt-get install jq
+sudo apt-get install zip
+
+sudo pip install sounddevice
+sudo pip install soundfile
+
