f4355fb0b739838cbb05b9d4fe663a4471a4bbf2
Andrew Booker <improvizone@gmail.com>
2022-06-01 20:24:43 +0100

    including the device colour in the ambient recording file name

--- record/ambient.sh
+++ record/ambient.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 for i in $(arecord --list-devices | grep USB | cut -d':' -f1 | cut -d' ' -f2)
 do
-	arecord -f S16_LE -r 44100 --device=hw:$i,0 -c1 ~/Music/ambient_card$(echo $i)_$(date +"%Y%m%d_%H%M%S").wav 2>/dev/null &
+    arecord -f S16_LE -r 44100 --device=hw:$i,0 -c1 ~/Music/$(jq -r .colour ~/Documents/static.json)_ambient_$(echo $i)_$(date +"%Y%m%d_%H%M%S").wav 2>/dev/null &
 done
