4abc19fafe5b5e2971e148c22f21ca1ce3300e97
Andrew Booker <improvizone@gmail.com>
2023-02-26 13:16:14 +0000

    extending controller api to get and set the tonic

--- serverless/controller.py
+++ serverless/controller.py
@@ -8,10 +8,26 @@ import sys
 import datetime
 from playPrepped import Player
 
+configLoc = sys.argv[2]
 def config(item):
-    with open(sys.argv[2]) as conf:
+    with open(os.path.join(configLoc, "config.json") as conf:
         c = json.load(conf)
         return c[item]
+
+class Key():
+    def __init__(self):
+        self.fqFn = os.path.join(configLoc, "key.json")
+        conf = open(self.fqFn)
+        self.key = json.load(conf)
+        conf.close()
+
+    def getTonic(self):
+        return self.key["tonic"]
+
+    def setTonic(self, t):
+        self.key["tonic"] = t
+        with open(self.fqFn, "w") as conf:
+            conf.write(json.dumps(self.key, indent=4))
 
 maxVol = 95
 player = Player(sys.argv[1], 3)
@@ -48,6 +64,7 @@ class PlayState():
         self.state = s
 
 volume = Volume(leftRelativeToRight)
+key = Key()
 playState = PlayState()
 
 
@@ -87,6 +104,9 @@ class Controller(BaseHTTPRequestHandler):
     def _setTime(self):
         os.system("sudo date --set %s" % self.headers.get("Current-Time"))
 
+    def _setTonic(self):
+        key.setTonic(int(self.headers.get("Tonic")))
+
     def _standardResponse(self):
         self.send_response(200)
         self.send_header("Content-Type", "application/json")
@@ -97,7 +117,8 @@ class Controller(BaseHTTPRequestHandler):
         self.wfile.write(json.dumps({
             "volume": int(volume.volume * volumeCoeff),
             "state": playState.get(),
-            "time": datetime.datetime.now().strftime("%d %b %Y %H:%M")
+            "time": datetime.datetime.now().strftime("%d %b %Y %H:%M"),
+            "tonic": key.getTonic()
         }).encode("utf-8"))
 
     def _sendVol(self):
@@ -109,6 +130,7 @@ class Controller(BaseHTTPRequestHandler):
         self._standardResponse()
         self.send_header("Access-Control-Allow-Methods", "GET, POST")
         self.send_header("Access-Control-Allow-Headers", "Current-Time")
+        self.send_header("Access-Control-Allow-Headers", "Tonic")
         self.end_headers()
 
     def do_GET(self):
