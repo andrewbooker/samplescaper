a534b067a2f6b36cb60c2b6f15e51fd07cd2ec68
Andrew Booker <improvizone@gmail.com>
2024-04-28 19:03:18 +0100

    adding instructions to set up RPi as a Randomatone

--- serverless/setup.sh
+++ serverless/setup.sh
@@ -1,11 +1,17 @@
 #!/bin/bash
 
-
-sudo apt-get install -y fswebcam
+sudo apt-get install -y vim
 sudo apt-get install -y python-pygame
 sudo apt-get install -y jq
 sudo apt-get install -y zip
-
+sudo ln -fs /usr/bin/python3.7 /usr/bin/python
 sudo pip install sounddevice
 sudo pip install soundfile
+
+cd ~/Documents
+git clone http://github.com/andrewbooker/samplescaper.git
+git clone http://github.com/andrewbooker/wavmixer.git
+git clone http://github.com/andrewbooker/rotation.git
+
+sudo sed -i '/dtparam=audio=on/c\dtoverlay=hifiberry-dacplus  #dtparam=audio=on' /boot/config.txt
 
