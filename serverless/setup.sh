9e3f076cac7ae827693099b7c76239a99d4b91b1
Andrew Booker <improvizone@gmail.com>
2024-04-28 19:16:26 +0100

    adding instruction to force fixed IP address

--- serverless/setup.sh
+++ serverless/setup.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-
+ipn=19
 sudo apt-get install -y vim
 sudo apt-get install -y python-pygame
 sudo apt-get install -y jq
@@ -14,4 +14,9 @@ git clone http://github.com/andrewbooker/wavmixer.git
 git clone http://github.com/andrewbooker/rotation.git
 
 sudo sed -i '/dtparam=audio=on/c\dtoverlay=hifiberry-dacplus  #dtparam=audio=on' /boot/config.txt
+
+echo "interface wlan0" >> /etc/dhcpcd.conf
+echo "static ip_address=192.168.1.${ipn}" >> /etc/dhcpcd.conf
+echo "static routers=192.168.1.254" >> /etc/dhcpcd.conf
+echo "static domain_name_servers=192.168.1.254" >> /etc/dhcpcd.conf
 
