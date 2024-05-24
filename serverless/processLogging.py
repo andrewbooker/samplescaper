77e919bc38c17263089cacd3e5faadc75df53559
Andrew Booker <improvizone@gmail.com>
2024-05-24 10:12:30 +0100

    using system time instead of forcing UTC for clarity

--- serverless/processLogging.py
+++ serverless/processLogging.py
@@ -1,10 +1,9 @@
 import datetime
-from dateutil import tz
 import logging
 import os
 import sys
 
-ts = datetime.datetime.now(tz=tz.UTC).strftime("%Y-%m-%d_%H%M%S")
+ts = datetime.datetime.now().strftime("%Y-%m-%d_%H%M%S")
 log_fn = os.path.join(os.getenv("HOME"), "Documents", "logs", f"randomtone_serverless_{ts}.log")
 logging.basicConfig(filename=log_fn, level=logging.INFO)
 
