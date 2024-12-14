4e31d0e2f7b7e74a5581d62a72b71005b303f1ae
Andrew Booker <improvizone@gmail.com>
2024-12-14 16:00:52 +0000

    adding alternative mono sound source to single unit player that reads files from a pool

--- singleunit/play.py
+++ singleunit/play.py
@@ -2,12 +2,16 @@
 
 import sys
 import sounddevice as sd
+import soundfile as sf
 import struct
 import numpy as np
+import random
+import os
 
 
 device = int(sys.argv[1]) if len(sys.argv) > 1 else None
 level = float(sys.argv[2]) if len(sys.argv) > 2 else 0.3
+inDir = sys.argv[3] if len(sys.argv) > 3 else None
 
 if device is None:
     print(sd.query_devices())
@@ -21,7 +25,7 @@ class MonoSoundSource:
     def __init__(self):
         self.pos = 0
 
-    def read(self, size):
+    def advance(self, size):
         self.pos += size
         pass
 
@@ -33,14 +37,50 @@ class MonoSineSource(MonoSoundSource):
 
     def read(self, size):
         t = (self.pos + np.arange(size)) / samplerate
-        super().read(size)
+        super().advance(size)
         return level * np.sin(2 * np.pi * self.freq * t.reshape(-1, 1))
 
 
-sources = [
-    MonoSineSource(220),
-    MonoSineSource(441)
-]
+class MonoWavSource(MonoSoundSource):
+    def __init__(self, inDir):
+        super(MonoWavSource, self).__init__()
+        self.inDir = inDir
+        self.fileBuffer = None
+
+    def getFile(self):
+        if self.fileBuffer is None:
+            rawFiles = os.listdir(self.inDir)
+            if len(rawFiles) == 0:
+                return
+            random.shuffle(rawFiles)
+            selected = rawFiles[0]
+            data, _ = sf.read(os.path.join(self.inDir, selected))
+            self.fileBuffer = data
+            print(selected, data[:5])
+
+    def read(self, size):
+        self.getFile()
+        ret = self.fileBuffer[self.pos:self.pos + size]
+        super().advance(size)
+        shortfall = size - len(ret)
+        if shortfall < 1:
+            return ret
+        self.fileBuffer = None
+        self.pos = 0
+        return [ret[i] if i < len(ret) else 0.0 for i in range(size)]
+
+sources = []
+
+if inDir is not None:
+    sources.extend([
+        MonoWavSource(inDir),
+        MonoWavSource(inDir)
+    ])
+else:
+    sources.extend([
+        MonoSineSource(220),
+        MonoSineSource(441)
+    ])
 
 def callback(outdata, frames, time, status):
     if status:
