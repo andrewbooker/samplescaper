ab3490d2dfeb5ec69c2a83e98f5e69074b7e7b43
Andrew Booker <improvizone@gmail.com>
2024-12-22 19:42:47 +0000

    fixed playing time for single unit player

--- singleunit/play.py
+++ singleunit/play.py
@@ -7,11 +7,13 @@ import struct
 import numpy as np
 import random
 import os
+import time
 
 
 device = int(sys.argv[1]) if len(sys.argv) > 1 else None
 level = float(sys.argv[2]) if len(sys.argv) > 2 else 0.3
 inDir = sys.argv[3] if len(sys.argv) > 3 else None
+playingTimeMins = int(sys.argv[4]) if len(sys.argv) > 4 else 3
 
 if device is None:
     print(sd.query_devices())
@@ -42,6 +44,9 @@ class MonoSineSource(MonoSoundSource):
         super().advance(size)
         return level * np.sin(2 * np.pi * self.freq * t.reshape(-1, 1))
 
+    def isFinished(self):
+        return False
+
 
 class MonoWavSource(MonoSoundSource):
     def __init__(self, inDir):
@@ -49,6 +54,11 @@ class MonoWavSource(MonoSoundSource):
         self.inDir = inDir
         self.fileBuffer = None
         self.blockCountdown = 0
+        self.start = time.time()
+        self.hasFinished = False
+
+    def isFinished(self):
+        return self.hasFinished
 
     def getFile(self):
         rawFiles = os.listdir(self.inDir)
@@ -60,11 +70,15 @@ class MonoWavSource(MonoSoundSource):
         print(selected, f"in {leadIn:.2f}s")
         data, _ = sf.read(os.path.join(self.inDir, selected))
         self.fileBuffer = [0.0] * int(leadIn * samplerate)
-        self.fileBuffer.extend(data)
+        self.fileBuffer.extend([(level * d) for d in data])
 
     def read(self, size):
         if self.fileBuffer is None:
-            self.getFile()
+            if (time.time() - self.start) > (60 * playingTimeMins):
+                self.hasFinished = True
+            else:
+                self.getFile()
+            return [0.0] * size
 
         ret = self.fileBuffer[self.pos:self.pos + size]
         super().advance(size)
@@ -97,6 +111,10 @@ def callback(outdata, frames, time, status):
     outdata[:] = struct.pack(f"{len(sources) * frames}f", *block)
 
 with sd.RawOutputStream(samplerate=samplerate, blocksize=blocksize, device=device, channels=len(sources), dtype="float32", callback=callback):
-    input()
+    while True:
+        if all([s.isFinished() for s in sources]):
+            print("All sources finished")
+            exit(0)
+        time.sleep(3)
 
 
