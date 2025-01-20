385239c9145dd953aaf6d2d6ea3cf1a4ee1a0c96
Andrew Booker <improvizone@gmail.com>
2025-01-20 14:46:40 +0000

    working out the HDMI device in the run script

--- singleunit/demo.sh
+++ singleunit/demo.sh
@@ -1,2 +1,4 @@
-./play.py 3 0.3 ~/Music/pool/looped 8 1
-./recycle.sh ~/Music/pool
+device=$(~/Documents/samplescaper/singleunit/play.py | grep hdmi | grep -v hifi | cut -d' ' -f3)
+echo using device $device
+~/Documents/samplescaper/singleunit/play.py 3 0.3 ~/Music/pool/looped 8 1
+~/Documents/samplescaper/singleunit/recycle.sh ~/Music/pool
