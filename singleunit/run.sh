88fce0960f871cac3befeef5051406762a6646ec
Andrew Booker <improvizone@gmail.com>
2025-01-31 17:01:30 +0000

    adjusting run script for standard hdmi device

--- singleunit/run.sh
+++ singleunit/run.sh
@@ -1,4 +1,4 @@
-device=$(~/Documents/samplescaper/singleunit/play.py | grep hdmi | grep -v hifi | cut -d' ' -f3)
+device=$(~/Documents/samplescaper/singleunit/play.py | grep hdmi | cut -d' ' -f3)
 echo using device $device
-~/Documents/samplescaper/singleunit/play.py 3 0.3 ~/Music/pool/looped 8 1
+~/Documents/samplescaper/singleunit/play.py 3 0.3 ~/Music/pool/looped 8 5
 ~/Documents/samplescaper/singleunit/recycle.sh ~/Music/pool
