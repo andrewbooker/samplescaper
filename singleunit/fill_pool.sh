896889a5a3b1b4798ebf5dd538948dfbe6b4df07
Andrew Booker <improvizone@gmail.com>
2025-02-01 10:52:42 +0000

    writing synth and looper console output to log file

--- singleunit/fill_pool.sh
+++ singleunit/fill_pool.sh
@@ -2,7 +2,7 @@ mkdir -p ~/Music/archives
 mkdir -p ~/Music/pool/factory
 mkdir -p ~/Music/pool/raw
 mkdir -p ~/Music/pool/looped
-mkdir -p ~/Documents/logs
+log_dir=/var/log/samplescaper
 
-python ~/Documents/samplescaper/synth/synth.py ~/Music/pool ~/Documents/samplescaper/key.json ~/Documents/samplescaper/config.json &
-python ~/Documents/samplescaper/synth/loop.py ~/Music/pool mono
+python -u ~/Documents/samplescaper/synth/synth.py ~/Music/pool ~/Documents/samplescaper/key.json ~/Documents/samplescaper/config.json > $log_dir/synth.log 2>&1 &
+python -u ~/Documents/samplescaper/synth/loop.py ~/Music/pool mono > $log_dir/looper.log 2>&1
