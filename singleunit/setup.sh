eb0d517e58659a0a6eff5a21c24b3ff710b948c1
Andrew Booker <improvizone@gmail.com>
2025-02-18 23:13:00 +0000

    adapting setup script for older ubuntu

--- singleunit/setup.sh
+++ singleunit/setup.sh
@@ -1,8 +1,8 @@
 sudo apt update
 sudo apt install -y python3-pip
 sudo ln -fs /usr/bin/python3 /usr/bin/python
-sudo pip install --break-system-packages sounddevice
-sudo pip install --break-system-packages soundfile
+sudo pip install sounddevice
+sudo pip install soundfile
 sudo apt install -y libsdl2-mixer-2.0-0 libsdl2-2.0-0 libportaudio2
 
 sudo apt -y install vim
@@ -13,7 +13,7 @@ sudo apt -y install tig
 sudo apt -y install curl
 sudo apt -y install htop
 sudo apt -y install net-tools
-sudo apt -y install dhcpcd
+sudo apt -y install dhcpcd5
 
 sudo mkdir /var/log/samplescaper
 sudo chown $USER /var/log/samplescaper
