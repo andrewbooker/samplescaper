2c4e0775d0bd80984ae71759e51c56c07253cb4f
Andrew Booker <improvizone@gmail.com>
2025-02-23 15:54:02 +0000

    pulling out directories into variables

--- singleunit/run_tmux.sh
+++ singleunit/run_tmux.sh
@@ -1,9 +1,11 @@
+cmdDir="~/Documents/samplescaper/singleunit"
+logDir="/var/log/samplescaper"
 
-cmdL0="./fill_pool.sh & ./run.sh"
-cmdL1="tail -f /var/log/samplescaper/synth.log"
-cmdL2="tail -f /var/log/samplescaper/looper.log"
+cmdL0="$cmdDir/fill_pool.sh & $cmdDir/run.sh"
 cmdR0="htop"
-cmdR1="tail -f /var/log/samplescaper/player.log"
+cmdR1="tail -f $logDir/player.log"
+cmdL1="tail -f $logDir/synth.log"
+cmdL2="tail -f $logDir/looper.log"
 
 tmux new-session "$cmdL0"\; \
 split-window -h "$cmdR0"\; \
