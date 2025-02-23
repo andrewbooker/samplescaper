e41d7627c21aa605bf0e365eb817c6b9acdbe9ca
Andrew Booker <improvizone@gmail.com>
2025-02-23 15:44:56 +0000

    simplifying tmux setup to six windows by combining run commands into one small window

--- singleunit/run_tmux.sh
+++ singleunit/run_tmux.sh
@@ -1,10 +1,7 @@
-cmdPlay="./run.sh"
-cmdFillPool="./fill_pool.sh"
 
-cmdL0=$cmdFillPool
-cmdL1=$cmdPlay
-cmdL2="tail -f /var/log/samplescaper/synth.log"
-cmdL3="tail -f /var/log/samplescaper/looper.log"
+cmdL0="./fill_pool.sh & ./run.sh"
+cmdL1="tail -f /var/log/samplescaper/synth.log"
+cmdL2="tail -f /var/log/samplescaper/looper.log"
 cmdR0="htop"
 cmdR1="tail -f /var/log/samplescaper/player.log"
 
@@ -12,10 +9,9 @@ tmux new-session "$cmdL0"\; \
 split-window -h "$cmdR0"\; \
 select-pane -t 0 \; \
 split-window -v "$cmdL2"\; \
-split-window -v "$cmdL3"\; \
 select-pane -t 0 \; \
 split-window -v "$cmdL1"\; \
-select-pane -t 4 \; \
+select-pane -t 3 \; \
 split-window -v "$cmdR1"\; \
-select-pane -t 4 \; \
+select-pane -t 3 \; \
 split-window "cd ~; exec bash"
