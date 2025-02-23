4716277eb44115a24882a03c203b58357af5d0c0
Andrew Booker <improvizone@gmail.com>
2025-02-23 10:38:57 +0000

    tmux script to set up running randomatones in split panes

---
+++ singleunit/run_tmux.sh
@@ -0,0 +1,20 @@
+cmdPlay="./play.py 7 0.3 ~/Music/pool/looped 8"
+cmdFillPool="./fill_pool.sh"
+
+
+cmdL0=$cmdFillPool
+cmdL1=$cmdPlay
+cmdL2="tail -f /var/log/samplescaper/synth.log"
+cmdL3="tail -f /var/log/samplescaper/looper.log"
+cmdR0="htop"
+cmdR1="tail -f /var/log/samplescaper/player.log"
+
+tmux new-session "$cmdL0"\; \
+split-window -h "$cmdR0"\; \
+select-pane -t 0 \; \
+split-window -v "$cmdL2"\; \
+split-window -v "$cmdL3"\; \
+select-pane -t 0 \; \
+split-window -v "$cmdL1"\; \
+select-pane -t 4 \; \
+split-window -v "$cmdR1"\; \
