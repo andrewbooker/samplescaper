7baf9800e0bafab70a4d1d80426d3493ffa782fc
Andrew Booker <improvizone@gmail.com>
2025-03-29 17:45:06 +0000

    starting to put together asound conf based on maestro PCI card

--- singleunit/cpp/gen_asound.cpp
+++ singleunit/cpp/gen_asound.cpp
@@ -1,9 +1,30 @@
 #include <fstream>
+#include <vector>
 
 int main() {
+    const std::vector<int> cards {1, 2};
+    const std::vector<int> subdevices {0};
+    const int deviceChannels(2);
+    const std::string name("maestro");
+    const std::string sp("  ");
+
     std::ofstream out("asound.conf", std::ofstream::out);
 
-    out << "{}\n";
-   
+    out << "pcm." << name << " {\n";
+
+    out << "}";
+
+    out << "\n";
+    out << "\n";
+
+    out << "pcm.randomatones {\n";
+    out << sp << "type route\n";
+    out << sp << "slave.pcm \"" << name << "\"\n";
+    const unsigned int channels(deviceChannels * cards.size() * subdevices.size());
+    for (unsigned int c(0); c != channels; ++c) {
+        out << sp << "ttable." << c << "." << c << " 1\n";
+    }
+    out << "}";
+    out << "\n";
     return 0;
 }
