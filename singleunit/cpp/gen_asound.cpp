69ba0dbcb0180445abf613af7f8a29ffda4138e2
Andrew Booker <improvizone@gmail.com>
2025-03-30 11:09:59 +0000

    splitting out card definition for referencing it by name

--- singleunit/cpp/gen_asound.cpp
+++ singleunit/cpp/gen_asound.cpp
@@ -4,22 +4,36 @@
 
 int main() {
     const std::vector<int> cards {0};
+    const std::string cardName("Audigy2");
     const std::vector<int> devices {0};
     const unsigned int deviceChannels(2);
     const std::string name("soundblaster");
     const std::string sp("  ");
 
+    const unsigned int channels(deviceChannels * cards.size() * devices.size());
     std::ofstream out("asound.conf", std::ofstream::out);
+
+    for (std::vector<int>::const_iterator d(devices.begin()); d != devices.end(); ++d) {
+        out << "pcm." << name << *d << " {\n";
+        out << sp << "type hw\n";
+        out << sp << "card \"" << cardName << "\"\n";
+        out << sp << "device " << *d << "\n";
+        out << "}";
+	out << "\n\n";
+    }
+
+    std::stringstream slaveName;
+    slaveName << name << "_" << channels << "ch";
 
     std::vector<std::string> bindings;
 
-    out << "pcm." << name << " {\n";
+    out << "pcm." << slaveName.str() << " {\n";
     out << sp << "type multi\n";
     for (unsigned int c(0); c != cards.size(); ++c) {
         for (unsigned int d(0); d != devices.size(); ++d) {
             const char slave('a' + c);
             out << sp << "slaves." << slave << " {\n";
-            out << sp << sp << "pcm \"hw:" << cards[c] << "," << devices[d] << ",0\"\n";
+            out << sp << sp << "pcm \"" << name << devices[d] << "\"\n";
             out << sp << sp << "channels " << deviceChannels << "\n";
             out << sp << "}\n";
             for (unsigned int ch(0); ch != deviceChannels; ++ch) {
@@ -42,8 +56,7 @@ int main() {
 
     out << "pcm.randomatones {\n";
     out << sp << "type route\n";
-    out << sp << "slave.pcm \"" << name << "\"\n";
-    const unsigned int channels(deviceChannels * cards.size() * devices.size());
+    out << sp << "slave.pcm \"" << slaveName.str() << "\"\n";
     for (unsigned int c(0); c != channels; ++c) {
         out << sp << "ttable." << c << "." << c << " 1\n";
     }
