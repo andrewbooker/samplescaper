6666137a06bfff97280bb7fc2ee147a3678edb90
Andrew Booker <improvizone@gmail.com>
2025-04-16 22:13:34 +0100

    pulling sound source abstract class into separate header file

---
+++ singleunit/cpp/soundSource.h
@@ -0,0 +1,55 @@
+#include <sndfile.h>
+#include <thread>
+#include <iostream>
+
+class SoundSource {
+private:
+    bool ready;
+    bool closing;
+    SNDFILE* soundFile;
+    std::thread loop;
+
+    void fetch() {
+        while (!closing) {
+            if (!ready) {
+                if (soundFile) {
+                    sf_close(soundFile);
+                    soundFile = 0;
+                }
+                soundFile = getSoundFile();
+                ready = true;
+            } else {
+               std::this_thread::sleep_for(std::chrono::milliseconds(500));
+            }
+        }
+    }
+
+    static void fetchLoop(SoundSource* s) {
+        s->fetch();
+    }
+
+protected:
+    virtual SNDFILE* getSoundFile() = 0;
+
+public:
+    SoundSource() : ready(false), closing(false), soundFile(0), loop(fetchLoop, this) {}
+    virtual ~SoundSource() {
+        if (soundFile) {
+            sf_close(soundFile);
+        }
+        closing = true;
+        std::cout << "Stopping fetch loop... ";
+        loop.join();
+        std::cout << "stopped\n";
+    }
+
+    void readInto(float* out, const unsigned long sampleLength) {
+        if (!ready) {
+            memset(out, 0, sampleLength * sizeof(float));
+            return;
+        }
+        const unsigned long read(sf_readf_float(soundFile, out, sampleLength));
+        ready = (read == sampleLength);
+    }
+};
+
