3491d84e795dc5778619fd37a91ef67803a73e82
Andrew Booker <improvizone@gmail.com>
2025-04-18 15:49:08 +0100

    adding frequency modulation

--- synth/http/py/server.py
+++ synth/http/py/server.py
@@ -66,16 +66,26 @@ class SineVariation(Envelope):
         return l + v
 
 
+class Modulation(Envelope):
+    def __init__(self, central_value, f, depth):
+        self.c = central_value
+        self.d = depth
+        self.f = f
+
+    def at(self, i):
+        return self.c + (self.d * (1.0 + math.sin(2 * math.pi * self.f * i / SAMPLE_RATE)))
+
+
 class SampleServer(BaseHTTPRequestHandler):
     def do_GET(self):
         size = int(SAMPLE_RATE * anywhere_between(8, 20))
         note = int(parse_qs(urlparse(self.path).query)["note"][0])
         envelope = LinearRampUpDown(size)
-        f = freq(note)
+        mf = Modulation(freq(note), anywhere_between(0.1, 2.1), anywhere_between(0.01, 0.08))
         am_depth = SineVariation(Constant(anywhere_between(0.1, 10.0)), (anywhere_between(0.005, 0.3), anywhere_between(0.4, 1.0)))
         am_freq = Constant(anywhere_between(0.3, 7.0))
         am = CosineAttenuation(am_freq, am_depth)
-        buff = [am.at(i) * envelope.at(i) * math.sin(2 * math.pi * f * i / SAMPLE_RATE) for i in range(size)]
+        buff = [am.at(i) * envelope.at(i) * math.sin(2 * math.pi * mf.at(i) * i / SAMPLE_RATE) for i in range(size)]
         self.send_response(200)
         self.send_header("Content-Type", "application/octet-stream")
         self.send_header("Content-Length", size * 4)
