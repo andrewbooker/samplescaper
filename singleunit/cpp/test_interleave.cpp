145d708394fe302bf289ac01a99d9d2adf6a8ced
Andrew Booker <improvizone@gmail.com>
2025-04-19 15:49:23 +0100

    demonstrating interleave over 6 channels in C++ test using common algorithm

--- singleunit/cpp/test_interleave.cpp
+++ singleunit/cpp/test_interleave.cpp
@@ -56,17 +56,23 @@ testOver(26)
 testOver(28)
 testOver(40)
 
+
+void populate(float* orig, float* expected, const int size, const int channels) {
+    const int perBlock(size / channels);
+    for (int i(0); i != size; ++i) {
+        const int c(i % channels);
+        const int p(i / channels);
+        orig[i] = 1.0 * i;
+        expected[i] = (1.0 * c * perBlock) + p;
+    }
+}
+
+
+
 TEST(InterleaveTest, over12ElementsAcross3Channels) {
     float buff[12] {};
     float expected[12] {};
-    const int channels(3);
-    const int perBlock(12 / channels);
-    for (int i(0); i != 12; ++i) {
-        const int c(i % channels);
-        const int p(i / channels);
-        buff[i] = 1.0 * i;
-        expected[i] = (1.0 * c * perBlock) + p;
-    }
+    populate(buff, expected, 12, 3);
     float test[12] {0, 4, 8, 1, 5, 9, 2, 6, 10, 3, 7, 11};
     for (int i(0); i != 12; ++i) {
         EXPECT_EQ(*(expected + i), *(test + i));
@@ -76,18 +82,20 @@ TEST(InterleaveTest, over12ElementsAcross3Channels) {
 TEST(InterleaveTest, over12ElementsAcross4Channels) {
     float buff[12] {};
     float expected[12] {};
-    const int channels(4);
-    const int perBlock(12 / channels);
-    for (int i(0); i != 12; ++i) {
-        const int c(i % channels);
-        const int p(i / channels);
-        buff[i] = 1.0 * i;
-        expected[i] = (1.0 * c * perBlock) + p;
-    }
+    populate(buff, expected, 12, 4);
     const float test[12] {0, 3, 6, 9, 1, 4, 7, 10, 2, 5, 8, 11};
     for (int i(0); i != 12; ++i) {
         EXPECT_EQ(*(expected + i), *(test + i));
     }
 }
 
+TEST(InterleaveTest, over18ElementsAcross6Channels) {
+    float buff[18] {};
+    float expected[18] {};
+    populate(buff, expected, 18, 6);
+    const float test[18] {0, 3, 6, 9, 12, 15, 1, 4, 7, 10, 13, 16, 2, 5, 8, 11, 14, 17};
+    for (int i(0); i != 18; ++i) {
+        EXPECT_EQ(*(expected + i), *(test + i));
+    }
+}
 
