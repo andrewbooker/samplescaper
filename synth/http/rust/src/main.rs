95793de961adcd9526bc156699c2f15fba069aa5
Andrew Booker <improvizone@gmail.com>
2025-04-21 10:33:46 +0100

    basic one-volume wave generation

--- synth/http/rust/src/main.rs
+++ synth/http/rust/src/main.rs
@@ -4,6 +4,38 @@ use std::{
     io::{BufReader, BufRead, Write}
 };
 use regex::Regex;
+use rand::Rng;
+
+
+const SAMPLE_RATE: u16 = 44100;
+
+
+struct Synth {
+    buffer: Vec<f32>
+}
+
+trait Generator {
+    fn new() -> Synth;
+    fn generate(&mut self, note: u8) -> &Vec<f32>;
+}
+
+
+impl Generator for Synth {
+    fn new() -> Synth {
+        let mut rng = rand::rng();
+        Synth {
+            buffer: vec![0.0; (SAMPLE_RATE as f32 * rng.random_range(8.0..20.0)) as usize]
+        }
+    }
+    fn generate(&mut self, note: u8) -> &Vec<f32> {
+        let freq = f32::powf(2.0, (note as i8 - 69) as f32 / 12.0) * 440.0;
+        let ang_freq = freq * 2.0 * std::f32::consts::PI;
+        for i in 0..self.buffer.len() {
+            self.buffer[i] = (i as f32 * ang_freq / SAMPLE_RATE as f32).sin();
+        }
+        &self.buffer
+    }
+}
 
 
 fn main() {
@@ -30,11 +62,17 @@ fn main() {
         let re = Regex::new(r"^([A-Z]+) /[\?]?([a-z]+)=?([0-9]*)").unwrap();
         for (_, [method, param, value]) in re.captures_iter(&http_request[0]).map(|c| c.extract()) {
             println!("{method} {param} {value}");
-        }
 
-        let status_line = "HTTP/1.1 200 OK";
-        let length = 0;
-        let response = format!("{status_line}\r\nContent-Type: application/octet-stream\r\nContent-Length: {length}\r\n\r\n");
-        stream.write_all(response.as_bytes()).unwrap();
+            let mut synth = Synth::new();
+            let note = value.parse::<u8>().unwrap();
+            let synth_buffer = synth.generate(note);
+
+            let status_line = "HTTP/1.1 200 OK";
+            let length = synth_buffer.len() * std::mem::size_of::<f32>();
+            let response = format!("{status_line}\r\nContent-Type: application/octet-stream\r\nContent-Length: {length}\r\n\r\n");
+            stream.write_all(response.as_bytes()).unwrap();
+
+            unsafe { stream.write_all(std::slice::from_raw_parts(synth_buffer.as_ptr() as *const u8, length)).unwrap() }
+        }
     }
 }
