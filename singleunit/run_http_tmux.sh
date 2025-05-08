6d0d3cc8bc5d21e9f983f0b6e8e01d38aec629e5
Andrew Booker <improvizone@gmail.com>
2025-05-08 16:34:38 +0000

    dynamically building http synth server tmux panes in bash

--- singleunit/run_http_tmux.sh
+++ singleunit/run_http_tmux.sh
@@ -1,22 +1,35 @@
 baseDir="~/Documents/samplescaper"
 synthDir="$baseDir/synth/http"
 playDir="$baseDir/singleunit/cpp"
-
 device=$(~/Documents/samplescaper/singleunit/play.py | sed -nE 's/\s*([0-9]+) randomatones, ALSA.*/\1/p')
-cmdR0="htop"
-cmdR1="cd $playDir; ./run_player.sh 6 $device 9966 9967 9968 9969 2>/dev/null"
-cmdL0="cd $synthDir/cpp; echo 'c++'; ./run.sh 9966"
-cmdL1="cd $synthDir/rust; echo 'rust'; ./run.sh 9967"
-cmdL2="cd $synthDir/php; echo 'php'; ./run.sh 9968"
-cmdL3="cd $synthDir/go; echo 'go'; ./run.sh 9969"
 
-tmux new-session "$cmdL0"\; \
-split-window -h "$cmdR0"\; \
-select-pane -t 0 \; \
-split-window -v "$cmdL2"\; \
-select-pane -t 0 \; \
-split-window -v "$cmdL1"\; \
-select-pane -t 2 \; \
-split-window -v "$cmdL3"\; \
-select-pane -t 4 \; \
-split-window -v -l '80%' "$cmdR1"\;
+basePort=9960
+synths=("cpp" "rust" "go" "php" "py")
+ports=()
+synthCmds=()
+
+for s in ${synths[@]}; do
+    ports+=($basePort)
+    synthCmds+=("cd $synthDir/$s; echo '$s'; ./run.sh $basePort")
+    ((++basePort))
+done
+
+cmdR0="htop"
+cmdR1="cd $playDir; ./run_player.sh 6 $device ${ports[@]} 2>/dev/null"
+sh=$((100 / ${#ports[@]}))
+
+tmuxCmds=()
+tmuxCmds+=("tmux new-session \"${synthCmds[0]}\"\;")
+tmuxCmds+=("split-window -h \"$cmdR0\"\;")
+
+for ((i=1; i < ${#synthCmds[@]}; ++i)); do
+    pc=$((100 * sh / (100 - ((i - 1) * sh))))
+    tmuxCmds+=("select-pane -t 0 \; split-window -v -l '$pc%' \"${synthCmds[i]}\"\;")
+done
+
+tmuxCmds+=("select-pane -t ${#ports[@]} \; split-window -v -l '80%' \"$cmdR1\"")
+
+echo "${tmuxCmds[@]}" > _gen.sh
+chmod +x _gen.sh
+./_gen.sh
+rm _gen.sh
