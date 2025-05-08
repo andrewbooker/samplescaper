07a36b80b0c1575d5074d2c3f747ac539e5e0fce
Andrew Booker <improvizone@gmail.com>
2025-05-08 17:20:07 +0000

    better arithmetic for working out synth pane heights in tmux

--- singleunit/run_http_tmux.sh
+++ singleunit/run_http_tmux.sh
@@ -1,13 +1,13 @@
+synths=("cpp" "py" "php" "perl" "rust" "go")
+
 baseDir="~/Documents/samplescaper"
 synthDir="$baseDir/synth/http"
 playDir="$baseDir/singleunit/cpp"
 device=$(~/Documents/samplescaper/singleunit/play.py | sed -nE 's/\s*([0-9]+) randomatones, ALSA.*/\1/p')
-
 basePort=9960
-synths=("cpp" "rust" "go" "php" "py")
+
 ports=()
 synthCmds=()
-
 for s in ${synths[@]}; do
     ports+=($basePort)
     synthCmds+=("cd $synthDir/$s; echo '$s'; ./run.sh $basePort")
@@ -16,14 +16,13 @@ done
 
 cmdR0="htop"
 cmdR1="cd $playDir; ./run_player.sh 6 $device ${ports[@]} 2>/dev/null"
-sh=$((100 / ${#ports[@]}))
-
 tmuxCmds=()
 tmuxCmds+=("tmux new-session \"${synthCmds[0]}\"\;")
 tmuxCmds+=("split-window -h \"$cmdR0\"\;")
 
-for ((i=1; i < ${#synthCmds[@]}; ++i)); do
-    pc=$((100 * sh / (100 - ((i - 1) * sh))))
+for ((i=1; i < ${#synths[@]}; ++i)); do
+    calc="100 * (1.0 / (${#synths[@]} - ($i - 1)))"
+    pc=$(awk "BEGIN {print int(0.5 + ($calc))}")
     tmuxCmds+=("select-pane -t 0 \; split-window -v -l '$pc%' \"${synthCmds[i]}\"\;")
 done
 
