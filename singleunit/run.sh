55bb781e707898d6e78fe13bc022066dbed95f48
Andrew Booker <improvizone@gmail.com>
2025-05-13 20:35:39 +0000

    getting loopback recording working for the python version

--- singleunit/run.sh
+++ singleunit/run.sh
@@ -1,3 +1,3 @@
-device=$(~/Documents/samplescaper/singleunit/play.py | sed -nE 's/.*([0-9]).+hdmi.*/\1/p')
+device=$(~/Documents/samplescaper/singleunit/play.py | sed -nE 's/\s*([0-9]+) randomatones, ALSA.*/\1/p')
 echo using device $device
 ~/Documents/samplescaper/singleunit/play.py $device 0.3 ~/Music/pool/looped 8
--- singleunit/run_hdmi_tmux.sh
+++ singleunit/run_hdmi_tmux.sh
@@ -1,19 +1,31 @@
 cmdDir="~/Documents/samplescaper/singleunit"
 logDir="/var/log/samplescaper"
 
-cmdL0="$cmdDir/fill_pool.sh & $cmdDir/run.sh"
+echo '' > $logDir/player.log
+echo '' > $logDir/synth.log
+echo '' > $logDir/looper.log
+rm  ~/Music/pool/raw/*.wav
+rm  ~/Music/pool/factory/*.wav
+rm  ~/Music/pool/looped/*.wav
+recording=~/Music/randomatones_$(date +"%Y%m%d_%H%M%S").wav
+cmdR2="$cmdDir/fill_pool.sh & $cmdDir/run.sh"
 cmdR0="htop"
-cmdR1="tail -f $logDir/player.log"
-cmdL1="tail -f $logDir/synth.log"
-cmdL2="tail -f $logDir/looper.log"
+cmdR1="cd ~; exec bash"
+cmdR3="tail -f $logDir/player.log"
+cmdL0="tail -f $logDir/synth.log"
+cmdL1="tail -f $logDir/looper.log"
+cmdL2="sleep 3; ffmpeg -f alsa -channels 2 -sample_rate 44100 -i loopout -y $recording"
 
 tmux new-session "$cmdL0"\; \
 split-window -h "$cmdR0"\; \
 select-pane -t 0 \; \
-split-window -v "$cmdL2"\; \
+split-window -v -l '20%' "$cmdL2"\; \
 select-pane -t 0 \; \
-split-window -v "$cmdL1"\; \
+split-window -v -l '50%' "$cmdL1"\; \
+select-pane -t 3 \; \
+split-window -v "$cmdR3"\; \
 select-pane -t 3 \; \
 split-window -v "$cmdR1"\; \
-select-pane -t 3 \; \
-split-window "cd ~; exec bash"
+select-pane -t 4 \; \
+split-window -v "$cmdR2"\; \
+select-pane -t 4
