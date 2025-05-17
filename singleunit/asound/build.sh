b407b71c389e3ba44fd15d2a0a93ccbe14ada713
Andrew Booker <improvizone@gmail.com>
2025-05-17 20:32:55 +0100

    first attempt at a bash script to generate an asound.conf for mixing multiple input channels to multiple output devices

---
+++ singleunit/asound/build.sh
@@ -0,0 +1,68 @@
+#!/usr/bin/bash
+
+declare -A cards=()
+cards["hdmi"]=8
+cards["front"]=2
+cards["loopin"]=2
+inputChannels=8
+indent='    '
+
+plug() {
+    name=$1
+    dev="$2"
+    echo "pcm.$name {"
+	echo "${indent}type plug"
+	echo "${indent}slave.pcm \"$dev\""
+    echo '}'
+    echo ''
+}
+
+
+echo "pcm.multi {"
+echo "${indent}type route"
+echo "${indent}slave.pcm {"
+echo "${indent}${indent}type multi"
+
+outputs=${#cards[@]}
+declare -A slaves=()
+letter='a'
+
+for c in ${!cards[@]}; do
+    ch=${cards[$c]}
+    slaves["${letter}"]=$ch
+
+    echo "${indent}${indent}slaves.${letter}.pcm \"${c}\""
+    echo "${indent}${indent}slaves.${letter}.channels $ch"
+
+    v=$(printf '%d' "'$letter")
+    ((v+=1))
+    letter=$(printf "\x$(printf %x $v)")
+done
+
+binding=0
+ttables=()
+slave=0
+for s in ${!slaves[@]}; do
+    ch=${slaves[$s]}
+    for ((i=0; i<$ch; ++i)); do
+        echo "${indent}${indent}bindings.${binding}.slave $s"
+        echo "${indent}${indent}bindings.${binding}.channel $i"
+        ((binding++))
+    done
+
+    gain=$(awk "BEGIN {print ($ch / $inputChannels)}")
+    for ((i=0; i<$inputChannels; ++i)); do
+        ttables+=("${i}.$((i % ch)) ${gain}")
+    done
+done
+
+echo "${indent}}"
+for t in "${ttables[@]}"; do
+    echo "${indent}ttable.$t"
+done
+
+echo "}"
+echo ''
+plug randomatones multi
+plug loopin "hw:Loopback,0,0"
+plug loopout "hw:Loopback,1,0"
