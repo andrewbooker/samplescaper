e47a2ba186d6f42c1f51488d5eedf009df833bec
Andrew Booker <improvizone@gmail.com>
2025-05-17 14:19:48 +0100

    passing the tonic to the rotation controller

--- serverless/auto_start.sh
+++ serverless/auto_start.sh
@@ -26,5 +26,5 @@ cd -
 ~/Documents/samplescaper/record/ambient.sh
 python ~/Documents/samplescaper/record/remoteCtlCamera.py &
 python ~/Documents/samplescaper/serverless/detect_auto.py $LED $BUTTON &
-python ~/Documents/rotation/propellorServo.py $(jq .isPilot ~/Documents/static.json) &
+python ~/Documents/rotation/propellorServo.py $(jq .isPilot ~/Documents/static.json) $(jq .tonic ~/Documents/samplescaper/key.json) &
 ~/Documents/samplescaper/serverless/start.sh
