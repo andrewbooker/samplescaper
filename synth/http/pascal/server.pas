268441bd709ef5798963e8ab96be0f821c930887
Andrew Booker <improvizone@gmail.com>
2025-05-31 11:01:46 +0100

    getting a note-independent sinewave back from the pascal synth server

--- synth/http/pascal/server.pas
+++ synth/http/pascal/server.pas
@@ -1,4 +1,6 @@
 program SynthServer;
+
+
 uses
     SysUtils, Sockets;
 
@@ -9,26 +11,54 @@ const
     responseCode: String = 'HTTP/1.1 200 OK';
     contentType: String = 'Content-Type: application/octet-stream';
     crlf: String = #13#10;
+    sampleRate: LongInt = 44100;
 
 var
     buffer: array[0..1023] of Char;
     received, accepted: LongInt;
     request, note, response: String;
-    ParamStart, ParamEnd, sent: Integer;
+    ParamStart, ParamEnd: Integer;
+    sampleTime: real;
+    sampleLength: longInt;
+    sampleByteLength: longInt;
+    sample: single;
+    idx: longInt;
+    byteLength: longInt;
+    responseBytes: array of byte;
+    preambleLength: integer;
 
 begin
+    Randomize;
     accepted := fpAccept(socket, nil, nil);
     fillChar(buffer, SizeOf(buffer), 0);
     received := fpRecv(accepted, @buffer, sizeOf(Buffer), 0);
-    writeLn('received ', received, ' bytes');
     request := copy(buffer, 1, received);
     ParamStart := pos(lookFor, request) + length(lookFor);
     ParamEnd := pos(' ', copy(request, ParamStart, length(request))) - 1;
     note := copy(request, ParamStart, ParamEnd);
+
     writeLn('Generating note ', note);
-    response := responseCode + crlf + contentType + crlf + 'Content-Length: 3' + crlf + crlf + 'OK' + #10;
-    sent := fpSend(accepted, @response + 1, length(response), 0);
-    writeLn(sent, ' bytes sent');
+    sampleTime := 8.0 + (random * 12.0);
+    writeLn(sampleTime:0:2, 's');
+    sampleLength := round(sampleTime * sampleRate);
+    writeLn(sampleLength, ' samples');
+    sampleByteLength := sampleLength * sizeOf(single);
+
+    response := responseCode + crlf + contentType + crlf + 'Content-Length: ' + intToStr(sampleByteLength) + crlf + crlf;
+    preambleLength := integer(response[0]);
+
+    byteLength := integer(response[0]) + sampleByteLength;
+    setLength(responseBytes, byteLength);
+    move(response[1], responseBytes[0], preambleLength);
+
+    for idx := 0 to sampleLength - 1 do
+    begin
+        sample := sin(idx * 800.0 / sampleRate);
+        move(sample, responseBytes[preambleLength + (idx * sizeOf(single))], sizeOf(single));
+    end;
+
+    fpSend(accepted, @responseBytes[0], byteLength, 0);
+    closeSocket(accepted);
 end;
 
 
@@ -51,6 +81,7 @@ begin
 
     writeLn('Listening on port ', port);
     while True do respond(socket);
+    closeSocket(socket);
 end;
 
 var
