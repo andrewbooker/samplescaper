9fa5576dec02bee9048b8e06935c9d08742a20a4
Andrew_RPi_linux <improvizone@gmail.com>
2025-11-01 21:14:28 +0000

    demoing setting random values where the red channel gets lower duty cycle

--- utils/rgbselector/rgb.py
+++ utils/rgbselector/rgb.py
@@ -8,15 +8,17 @@ import random
 class RgbSelector:
     def __init__(self):
         self.freq = 400
-        self.port_numbers = [16]
+        self.port_numbers = [
+            (23, 67), (24, 100), (25, 100), (8, 100)
+        ]
         self.ports = []
         gpio.setmode(gpio.BCM)
         for p in self.port_numbers:
-            gpio.setup(p, gpio.OUT)
-            self.ports.append(gpio.PWM(p, self.freq))
-            print(f"port {p} PWM")
+            gpio.setup(p[0], gpio.OUT)
+            self.ports.append((gpio.PWM(p[0], self.freq), p[1]))
+            print(f"port {p[0]} PWM up to {p[1]}")
         for p in self.ports:
-            p.start(0)
+            p[0].start(0)
 
     def __del__(self):
         print("shutting down")
@@ -25,19 +27,19 @@ class RgbSelector:
     def set_to(self, v):
         print(f"setting value {v}")
         for p in self.ports:
-            p.ChangeDutyCycle(v)
+            p[0].ChangeDutyCycle(int(v * p[1] / 100.0))
 
     def set_value(self):
         for p in self.ports:
-            v = 10 + (90 * random.random())
+            v = int((5 + (95 * random.random())) * p[1] / 100.0)
             print(f"setting value {v}")
-            p.ChangeDutyCycle(v)
+            p[0].ChangeDutyCycle(v)
 
 
 selector = RgbSelector()
 time.sleep(1)
 for i in range(9):
-    selector.set_to((i + 1) * 10)
+    selector.set_value()
     time.sleep(2)
 
 del selector
