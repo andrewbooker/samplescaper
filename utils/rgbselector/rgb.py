6e461cd52e85348f6522394dd419d27573b625e0
Andrew Booker <improvizone@gmail.com>
2025-11-13 21:03:44 +0000

    adding web server to python rgb selector

--- utils/rgbselector/rgb.py
+++ utils/rgbselector/rgb.py
@@ -3,6 +3,9 @@
 import RPi.GPIO as gpio
 import time
 import random
+from http.server import HTTPServer, BaseHTTPRequestHandler
+import json
+import threading
 
 
 class RgbSelector:
@@ -12,11 +15,11 @@ class RgbSelector:
             (23, 67), (24, 100), (25, 100), (8, 100)
         ]
         self.ports = []
+        self.values = dict()
         gpio.setmode(gpio.BCM)
         for p in self.port_numbers:
             gpio.setup(p[0], gpio.OUT)
-            self.ports.append((gpio.PWM(p[0], self.freq), p[1]))
-            print(f"port {p[0]} PWM up to {p[1]}")
+            self.ports.append((gpio.PWM(p[0], self.freq), p[1], p[0]))
         for p in self.ports:
             p[0].start(0)
 
@@ -24,22 +27,49 @@ class RgbSelector:
         print("shutting down")
         gpio.cleanup()
 
-    def set_to(self, v):
-        print(f"setting value {v}")
-        for p in self.ports:
-            p[0].ChangeDutyCycle(int(v * p[1] / 100.0))
-
-    def set_value(self):
+    def set_values(self):
         for p in self.ports:
             v = int((5 + (95 * random.random())) * p[1] / 100.0)
-            print(f"setting value {v}")
+            self.values[p[2]] = v
             p[0].ChangeDutyCycle(v)
 
 
 selector = RgbSelector()
-time.sleep(1)
-for i in range(9):
-    selector.set_value()
-    time.sleep(2)
+PORT = 9988
 
+class Controller(BaseHTTPRequestHandler):
+    def __standardResponse(self):
+        self.send_response(200)
+        self.send_header("Content-Type", "application/json")
+        self.send_header("Access-Control-Allow-Origin", "*")
+        self.send_header("Access-Control-Allow-Headers", "Content-Type")
+
+    def do_OPTIONS(self):
+        self.__standardResponse()
+        self.send_header("Access-Control-Allow-Methods", "GET, POST")
+        self.end_headers()
+
+    def do_GET(self):
+        self.__standardResponse()
+        self.end_headers()
+        payload = {
+            "values": selector.values
+        }
+        self.wfile.write(json.dumps(payload).encode("utf-8"))
+
+    def do_PUT(self):
+        selector.set_values()
+        self.do_GET()
+
+
+def startServer():
+    HTTPServer(("0.0.0.0", PORT), Controller).serve_forever()
+
+
+threads = []
+threads.append(threading.Thread(target=startServer, args=(), daemon=True))
+[t.start() for t in threads]
+print("serving on port", PORT)
+
+[t.join() for t in threads]
 del selector
