09afd978797a9af4ecb23d8719bca49b4f646343
Andrew Booker <improvizone@gmail.com>
2025-12-23 23:30:49 +0000

    basic alternative to synth server that picks up sample files

---
+++ distributed/cpp/server.cpp
@@ -0,0 +1,242 @@
+#include <iostream>
+#include <cstring>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <unistd.h>
+#include <sstream>
+#include <string>
+#include <regex>
+#include <cmath>
+#include <math.h>
+#include <vector>
+#include <filesystem>
+#include <sndfile.h>
+
+
+#define SAMPLE_RATE 44100
+
+const float anywhereBetween(const float& l, const float& u) {
+    const float r(rand() * 1.0 / RAND_MAX);
+    return l + (std::pow(r * (u - l), 2.0) / u);
+}
+
+
+class Envelope {
+public:
+    virtual const float at(const unsigned long i) const = 0;
+};
+
+
+class ConstVal : public Envelope {
+    const float& val;
+
+public:
+    ConstVal(const float& v) : val(v) {}
+    const float at(const unsigned long i) const { return val; }
+};
+
+
+class Scaled : public Envelope {
+    const Envelope& envelope;
+    const float min;
+    const float scale;
+
+public:
+    Scaled(const Envelope& e, const float m, const float s) : envelope(e), min(m), scale(s) {}
+
+    const float at(const unsigned long i) const {
+        return min + (scale * envelope.at(i));
+    }
+};
+
+
+class AsPositive : public Envelope {
+    const Envelope& oscillator;
+
+public:
+    AsPositive(const Envelope& e) : oscillator(e) {}
+
+    const float at(const unsigned long i) const {
+        return 0.5 * (1.0 + oscillator.at(i));
+    }
+};
+
+
+class RampUpDown : public Envelope {
+    const unsigned long rampUp;
+    const unsigned long rampDown;
+    const unsigned long startRampDown;
+
+public:
+    RampUpDown(const unsigned long size) :
+        rampUp(SAMPLE_RATE * anywhereBetween(2.0, 4.0)),
+        rampDown(size * anywhereBetween(0.2, 0.5)),
+        startRampDown(size - rampDown)
+    {}
+
+    const float at(const unsigned long i) const {
+        if (i < rampUp) {
+            return 0.5 * (1.0 + cos(M_PI * (i + rampUp) / rampUp));
+        }
+        if (i > startRampDown) {
+            return 0.5 * (1.0 + cos(M_PI * (i - startRampDown) / rampDown));
+        }
+        return 1.0;
+    }
+};
+
+
+class Lfo : public Envelope {
+    const float freq;
+
+public:
+    Lfo() : freq(anywhereBetween(0.05, 5.5)) {}
+
+    const float at(const unsigned long i) const {
+        return sin(2 * M_PI * freq * i / SAMPLE_RATE);
+    }
+};
+
+
+
+class SampleFetcher {
+public:
+    typedef std::vector<float> t_sound;
+
+private:
+    typedef std::vector<std::string> t_fileNames;
+    t_sound buffer;
+    const std::string location;
+    std::string latest;
+
+public:
+    SampleFetcher(const char* loc) : location(loc), latest("") {}
+
+    const std::string& describeLatest() const {
+        return latest;
+    }
+
+    const t_sound& fetch() {
+        const float lengthSecs(anywhereBetween(8, 20));
+        const unsigned long size(SAMPLE_RATE * lengthSecs);
+        buffer.clear();
+        buffer.reserve(size);
+
+        const Lfo amLfo;
+        const AsPositive amLfoP(amLfo);
+        const float amDepth(anywhereBetween(0.0, 1.0));
+        const Scaled am(amLfo, amDepth, 1.0 - amDepth);
+
+        const RampUpDown amplitude(size);
+
+        t_fileNames fileNames;
+        for (const auto & entry : std::filesystem::directory_iterator(location)) {
+            fileNames.push_back(entry.path());
+        }
+        const unsigned int selection(rand() % fileNames.size());
+        SF_INFO info;
+        latest = fileNames[selection];
+        std::cout << "opening " << latest << std::endl;
+        SNDFILE* soundFile(sf_open(latest.c_str(), SFM_READ, &info));
+        const unsigned int sampleLength(info.frames);
+        std::cout << "expecting " << sampleLength << " frames" << std::endl;
+        t_sound fileBuffer;
+        fileBuffer.reserve(sampleLength);
+        const unsigned long read(sf_readf_float(soundFile, fileBuffer.data(), sampleLength));
+        sf_close(soundFile);
+        std::cout << "read " << read << " samples" << std::endl;
+        std::cout << "prepping " << size << " samples" << std::endl;
+        for (unsigned long i(0); i != size; ++i) {
+            const float sample(i < read ? fileBuffer[i] : 0.0);
+            buffer.push_back(am.at(i) * amplitude.at(i) * sample);
+        }
+
+        return buffer;
+    }
+};
+
+
+class Server {
+    const int server_fd;
+    const char* fileLoc;
+    struct sockaddr_in address;
+    socklen_t addrlen;
+
+public:
+    Server(const unsigned int port, const char* fl) : fileLoc(fl), addrlen(sizeof(address)), server_fd(socket(AF_INET, SOCK_STREAM, 0)) {
+        if (server_fd == 0) {
+            perror("Failed to create socket");
+            exit(EXIT_FAILURE);
+        }
+        int opt(1);
+        setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR, &opt, sizeof(opt));
+
+        address.sin_family = AF_INET;
+        address.sin_addr.s_addr = INADDR_ANY;
+        address.sin_port = htons(port);
+
+        if (bind(server_fd, reinterpret_cast<sockaddr*>(&address), sizeof(address)) < 0) {
+            perror("bind failed");
+            close(server_fd);
+            exit(EXIT_FAILURE);
+        }
+
+        if (listen(server_fd, 3) < 0) {
+            perror("listen failed");
+            close(server_fd);
+            exit(EXIT_FAILURE);
+        }
+    }
+
+    ~Server() {
+        if (isAlive()) {
+            std::cout << "Shutting down\n";
+            close(server_fd);
+        }
+    }
+
+    const bool isAlive() const {
+        return server_fd != 0;
+    }
+
+    bool listenLoop() {
+        int client_fd(accept(server_fd, reinterpret_cast<sockaddr*>(&address), &addrlen));
+        if (client_fd < 0) {
+            return true;
+        }
+        SampleFetcher fetcher(fileLoc);
+        const SampleFetcher::t_sound& sound(fetcher.fetch());
+        std::cout << "generating " << fetcher.describeLatest() << " for " << sound.size() * 1.0 / SAMPLE_RATE << "s\n";
+        const unsigned long binarySize(sound.size() * sizeof(float));
+        std::stringstream responseHeader;
+        responseHeader << "HTTP/1.1 200 OK\r\n" << "Content-Type: application/octet-stream\r\n" << "Content-Length: " << binarySize << "\r\n";
+        responseHeader << "\r\n";
+        const std::string& resp(responseHeader.str());
+        send(client_fd, resp.c_str(), resp.size(), 0);
+        send(client_fd, sound.data(), binarySize, 0);
+
+        close(client_fd);
+        return false;
+    }
+};
+
+
+int main(int argc, char* argv[]) {
+    srand(time(0));
+    if (argc < 2) {
+        std::cout << "Must supply port number" << std::endl;
+        return 0;
+    }
+    const unsigned int port(atoi(argv[1]));
+    const char* fileLoc(argv[2]);
+    Server server(port, fileLoc);
+    if (!server.isAlive()) {
+        return 1;
+    }
+    std::cout << "Server listening on port " << port << "...\n";
+    bool done(false);
+    while (!done) {
+        done = server.listenLoop();
+    }
+    return 0;
+}
