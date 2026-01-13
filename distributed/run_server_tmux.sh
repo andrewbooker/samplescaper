110f05f2c5ea7e57dc483dc58c8a4eb2f5e81add
Andrew Booker <improvizone@gmail.com>
2026-01-13 14:41:07 +0000

    rearranging the server demo to launch processes from the hanging speaker unit

--- distributed/run_server_tmux.sh
+++ distributed/run_server_tmux.sh
@@ -1,21 +1,38 @@
 #!/bin/bash
 set -e
 
-port=3064
+capturePort=3064
 captureBaseLoc='~/Music/pool'
 captureLoc=$captureBaseLoc/live
 captureCmd="~/Documents/samplescaper/distributed/pool.py $captureBaseLoc"
-converterCmd="~/Documents/samplescaper/distributed/cpp/bin/server $port $captureLoc"
-localPlayerCmd="~/Documents/samplescaper/singleunit/cpp/player 2 5 $port; bash -i"
+converterCmd="~/Documents/samplescaper/distributed/cpp/bin/server $capturePort $captureLoc"
 fileListCmd="watch -n1 'ls -lth $captureLoc'"
+synthCmd="cd ~/Documents/samplecaper/synth/http/ada; ./run.sh 9965"
+cameraCmd="~/Documents/mediautils/webcamShow.py 2"
 
+
+remoteHanging="ssh pi@192.168.1.99"
+remoteHangingMotors="cd ~/Documents/samplescaper/singleunit/motors; ./server.py"
+remoteHangingPlayer="cd ~/Documents/samplescaper/singleunit/cpp; ./run_player.sh 2 8 192.168.1.88:$capturePort"
+
+remoteBlue="ssh pi@192.168.1.19"
+blueClientLog="cd /var/log/randomatones; ./tail_last.sh"
+
+panes=0
 tmuxCmds=()
 tmuxCmds+=("tmux new-session \"$captureCmd\"\;")
-tmuxCmds+=("split-window -h \"htop\"\;")
-tmuxCmds+=("select-pane -t 0 \; split-window -v -l '50%' \"$converterCmd\"\;")
-tmuxCmds+=("select-pane -t 2 \; split-window -v -l '50%' \"$localPlayerCmd\"\;")
-tmuxCmds+=("select-pane -t 2 \; split-window -v -l '50%' \"$fileListCmd\"\;")
-tmuxCmds+=("select-pane -t 4")
+tmuxCmds+=("split-window -h \"$remoteHanging\"\;")
+tmuxCmds+=("select-pane -t 0 \; split-window -v -l '84%' \"$fileListCmd\"\;")
+tmuxCmds+=("select-pane -t 1 \; split-window -v -l '20%' \"$cameraCmd & htop\"\;")
+#tmuxCmds+=("select-pane -t 1 \; split-window -v -l '10%' \"$cameraCmd\"\;")
+tmuxCmds+=("select-pane -t 1 \; split-window -v \"$converterCmd\" \;")
+topRightPaneIdx=$((${#tmuxCmds[@]}-1))
+tmuxCmds+=("select-pane -t $topRightPaneIdx \; split-window -v -l '85%' \"$remoteHanging\" \;")
+tmuxCmds+=("select-pane -t $((topRightPaneIdx+1)) \; split-window -v \"$remoteHanging\" \;")
+
+tmuxCmds+=("send-keys -t $((topRightPaneIdx+2)) \"htop\" ENTER \;")
+tmuxCmds+=("send-keys -t $topRightPaneIdx \"$remoteHangingMotors\" ENTER \;")
+tmuxCmds+=("send-keys -t $((topRightPaneIdx+1)) \"$remoteHangingPlayer\" ENTER \;")
 
 echo "${tmuxCmds[@]}" > _gen.sh
 chmod +x _gen.sh
