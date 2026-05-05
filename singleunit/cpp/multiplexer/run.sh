16f758432cc0dc1cda602610484304e097fa8b0c
FS_2005 <improvizone@gmail.com>
2026-05-05 11:10:59 +0000

    only compiling the multiplexer there is no executable

--- singleunit/cpp/multiplexer/run.sh
+++ singleunit/cpp/multiplexer/run.sh
@@ -1,4 +1,8 @@
-set -e
-mkdir -p bin
-g++ server.cpp -l curl -o ./bin/server
+compile() {
+    echo 'compiling multiplexer'
+    set -e
+    mkdir -p bin
+    g++ server.cpp -l curl -o ./bin/server
+}
+[ -f ./bin/server ] || compile
 bin/server $1
