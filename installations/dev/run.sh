d0a7228f79af2d58f70d526c20a1aea83ce353c1
Andrew Booker <improvizone@gmail.com>
2026-05-27 22:36:40 +0100

    basic run script for laptop work

---
+++ installations/dev/run.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+set -e
+basePort=9960
+base="/home/$USER/Documents/samplescaper"
+synth="cd $base/synth/http/ada; ./run.sh $((basePort + 1))"
+player="cd $base/singleunit/cpp; ./run_player.sh 2 5 $basePort"
+multiplexer="cd $base/singleunit/cpp/multiplexer; ./run.sh $basePort"
+
+tmuxCmds=("tmux")
+tmuxCmds+=("new-session \"bash\"\;")
+tmuxCmds+=("split-window -h \"$player\"\;")
+tmuxCmds+=("select-pane -t 0 \; split-window -v -l '50%' \"$synth\"\;")
+tmuxCmds+=("select-pane -t 0 \; split-window -v -l '50%' \"$multiplexer\"\;")
+tmuxCmds+=("select-pane -t 2")
+
+echo "${tmuxCmds[@]}" > _gen_tmux.sh
+chmod +x _gen_tmux.sh
+./_gen_tmux.sh
+rm _gen_tmux.sh
+
