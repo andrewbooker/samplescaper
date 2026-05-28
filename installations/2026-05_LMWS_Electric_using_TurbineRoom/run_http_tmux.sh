1e3f6e0f4a86d87ac23ab704bb137aebbf176b75
Turbine Room <improvizone@gmail.com>
2026-05-28 18:46:18 +0000

    adding multiplexer to interim Electric Room setup

--- installations/2026-05_LMWS_Electric_using_TurbineRoom/run_http_tmux.sh
+++ installations/2026-05_LMWS_Electric_using_TurbineRoom/run_http_tmux.sh
@@ -20,9 +20,9 @@ basePort=9960
 ports=()
 synthCmds=()
 for s in ${synths[@]}; do
+    ((++basePort))
     ports+=($basePort)
     synthCmds+=("cd $synthDir/$s; echo '$s on $basePort'; ./run.sh $basePort")
-    ((++basePort))
 done
 
 
@@ -36,12 +36,14 @@ for ((i=1; i < ${#synths[@]}; ++i)); do
     tmuxCmds+=("select-pane -t 1 \; split-window -v -l '$pc%' \"${synthCmds[i]}\"\;")
 done
 
-playSize=80
+playSize=85
 
-cmdPlay="cd $playDir; ./player 8 $device ${ports[@]} 2>/dev/null"
-cmdAuto="sleep 10; watch -n30 './auto_start_stop 09:03 15:57'"
+cmdPlay="cd $playDir; ./player 8 $device $basePort 2>/dev/null"
+cmdMux="cd $playDir/multiplexer; ./run.sh $basePort"
+cmdAuto="sleep 10; watch -n30 './auto_start_stop 09:03 14:57'"
 tmuxCmds+=("select-pane -t 0 \; split-window -v -l '${playSize}%' \"$cmdPlay\"\;")
-tmuxCmds+=("select-pane -t 1 \; split-window -v -l '12%' \"$cmdAuto\"\;")
+tmuxCmds+=("select-pane -t 1 \; split-window -v -l '40%' \"$cmdMux\"\;")
+tmuxCmds+=("select-pane -t 2 \; split-window -v -l '30%' \"$cmdAuto\"\;")
 tmuxCmds+=("select-pane -t 1 \;")
 echo 'compiling player...'
 cd "$playDir"
