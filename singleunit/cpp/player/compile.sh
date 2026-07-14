c9760c58d8ee88212733d5af6fe4b947c816d621
Andrew Booker <improvizone@gmail.com>
2026-07-14 18:25:43 +0100

    adding player compile script

---
+++ singleunit/cpp/player/compile.sh
@@ -0,0 +1,10 @@
+set -e
+links=()
+links+=("-l sndfile")
+links+=("-l asound")
+links+=("-l portaudio")
+links+=("-l curl")
+if [[ ! -v $(uname -a | grep raspberry) ]]; then
+    links+=("-pthread")
+fi
+g++ player.cpp -o player ${links[@]}
